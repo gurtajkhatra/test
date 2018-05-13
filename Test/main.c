61910c0110c84004c1bbbb0070e194a6deeba876
gurtaj <khatra.g@husky.neu.edu>
2018-05-13 19:30:57 -0400

    test

--- Test/main.c
+++ Test/main.c
@@ -1,15 +1,24 @@
-//
-//  main.c
-//  Test
-//
-//  Created by Gurtaj Khatra on 10/20/15.
-//  Copyright © 2015 Gurtaj Khatra. All rights reserved.
-//
+/* See Chapter 5 of Advanced UNIX Programming:  http://www.basepath.com/aup/
+ *   for further related examples of systems programming.  (That home page
+ *   has pointers to download this chapter free.
+ *
+ * Copyright (c) Gene Cooperman, 2006; May be freely copied as long as this
+ *   copyright notice remains.  There is no warranty.
+ */
 
+/* To know which "includes" to ask for, do 'man' on each system call used.
+ * For example, "man fork" (or "man 2 fork" or man -s 2 fork") requires:
+ *   <sys/types.h> and <unistd.h>
+ */
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+int main(int argc, char *argv[])
+{
+    char test[100] = "The quick brown foc";
+    char *testPoint = test;
+    
+    printf("%c", *testPoint);
+    testPoint++;
+    printf("%c", *testPoint);
 }
